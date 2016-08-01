a9199d873c084c76cb58b897e92f6438c948f365
rladdusaw <rladdusaw@gmail.com>
2016-08-01 15:35:59 -0500

    applied bootstrap css to index.php

--- public/index.php
+++ public/index.php
@@ -2,9 +2,14 @@
 
 <head>
     <title>To-Do List</title>
+    <link href="/todolist/resources/static/bootstrap/css/bootstrap.min.css" rel="stylesheet">
 </head>
 <body>
-    <a href="./login.php">Login</a>
-    <br>
-    <a href="./register.php">Register</a>
+    <?php require_once("../resources/navbar.php") ?>
+    <div class="container col-md-6 col-md-offset-3">
+        <a class="btn btn-large btn-default" role="button" href="login.php">Login</a>
+        <a class="btn btn-large btn-default" role="button" href="register.php">Register</a>
+    </div>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="/todolist/resources/static/bootstrap/js/bootstrap.min.js"></script>
 </body>
